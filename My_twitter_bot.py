a3e61c642df051078bb534a97bb8abda6c0e895a
DN41-JH <liujianghuai0804@gmail.com>
2021-01-29 18:08:14 -0600

    Setting up personal Twitter developer keys and tokens

---
+++ My_twitter_bot.py
@@ -0,0 +1,12 @@
+import tweepy
+
+print("This is my automatically replying Twitter robot")
+
+API_KEY = '41JHa9mpYkeS0IyREeTQ9MKKZ'
+API_SECRET = '9tJUqNisMaWocqIMKJPhjZ0T1gqfSXrvwC9TTp2LRKjVnbST6s'
+ACCESS_TOKEN = '1355176972360495108-e6NdMoAObMtMifXnR3LtgLzzJ1iEJP'
+ACCESS_SECRET = '6kVGz3Mv5OIW4Id9brBj7m8RdEw22QQNtXwkNgbNWhSNG'
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
