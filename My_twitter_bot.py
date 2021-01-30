2eda7bcbbeae854be727703678015563fae080d3
DN41-JH <liujianghuai0804@gmail.com>
2021-01-29 18:51:35 -0600

    Test for extracting messager id and content

--- My_twitter_bot.py
+++ My_twitter_bot.py
@@ -10,3 +10,8 @@ ACCESS_SECRET = '6kVGz3Mv5OIW4Id9brBj7m8RdEw22QQNtXwkNgbNWhSNG'
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+	print('mention.id' + 'mention.full_text')
